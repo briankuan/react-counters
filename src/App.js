3d1683b978de502039ec218a48c416adb35c29e5
Brian Kuan <26177329+briankuan@users.noreply.github.com>
2020-07-16 09:28:08 +1000

    Decrement Button

--- src/App.js
+++ src/App.js
@@ -13,6 +13,14 @@ class App extends Component {
     ]
   };
 
+  handleDecrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value--;
+    this.setState({ counters });
+  };
+
   handleIncrement = counter => {
     const counters = [...this.state.counters];
     const index = counters.indexOf(counter);
@@ -45,6 +53,7 @@ class App extends Component {
             counters={this.state.counters}
             onReset={this.handleReset}
             onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
             onDelete={this.handleDelete}
           />
         </main>
