32aa5bfe5ad1dad62b9d66676523d3b70728ee20
Brian Kuan <26177329+briankuan@users.noreply.github.com>
2020-07-15 20:13:42 +1000

    Multiple Components in Sync

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import './styles.css';
+import NavBar from './components/navbar';
+import Counters from './components/counters';
+import './App.css';
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Counters />
+      </main>
+    </React.Fragment>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
-import Counters from './components/counters';
+import App from './App';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
   <React.StrictMode>
-    <Counters />
+    <App />
   </React.StrictMode>,
   rootElement
 );
